7b3c26f9f446c2338c8b465292df9c9928a1cd37
Kelvin-Clyde McGuire <kmcguir5@bulldogs.aamu.edu>
2020-03-01 19:42:50 -0600

    Part 2

---
+++ app/src/main/java/com/codepath/kmcguire/parseinstagram/fragments/ProfileFragment.java
@@ -0,0 +1,39 @@
+package com.codepath.kmcguire.parseinstagram.fragments;
+
+import android.util.Log;
+
+import com.codepath.kmcguire.parseinstagram.Post;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.List;
+
+public class ProfileFragment extends PostsFragment {
+
+    @Override
+    protected void queryPosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.whereEqualTo(Post.KEY_USER, ParseUser.getCurrentUser());
+        query.setLimit(20);
+        query.addDescendingOrder(Post.KEY_CREATED_KEY);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null){
+                    Log.e(TAG, "Error with query");
+                    e.printStackTrace();
+                    return;
+                }
+                for (int i = 0; i < posts.size(); i++) {
+                    Post post = posts.get(i);
+                    Log.d(TAG, "Post: " + posts.get(i).getDescription() + ", username: " + posts.get(i).getUser().getUsername());
+                }
+                allPosts.addAll(posts);
+                adapter.notifyDataSetChanged();
+            }
+        });
+    }
+}
